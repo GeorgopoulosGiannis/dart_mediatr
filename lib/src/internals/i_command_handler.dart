a3ba96c64933bb20530027336258bd49045143d0
Giannis Georgopoulos <gga@softone.gr>
2022-07-18 14:41:47 +0300

    Format

--- lib/src/internals/i_command_handler.dart
+++ lib/src/internals/i_command_handler.dart
@@ -2,6 +2,7 @@ import 'i_request.dart';
 
 import 'i_request_handler.dart';
 
-abstract class ICommandHandler<R extends IRequest<void>> extends IRequestHandler<void, R> {
+abstract class ICommandHandler<R extends IRequest<void>>
+    extends IRequestHandler<void, R> {
   ICommandHandler();
 }
